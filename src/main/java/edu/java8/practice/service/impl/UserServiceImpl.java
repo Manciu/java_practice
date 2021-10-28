04747dc665569759e894c40bf1474f49680a14df
marianburdianov <marian.burdianov@gmail.com>
2021-10-28 21:42:11 +0300

    sortByAgeDescAndNameAsc

--- src/main/java/edu/java8/practice/service/impl/UserServiceImpl.java
+++ src/main/java/edu/java8/practice/service/impl/UserServiceImpl.java
@@ -4,17 +4,24 @@ import edu.java8.practice.domain.Privilege;
 import edu.java8.practice.domain.User;
 import edu.java8.practice.service.UserService;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> sortByAgeDescAndNameAsc(final List<User> users) {
-        throw new UnsupportedOperationException("Not implemented");
+
+        return users.stream()
+                .sorted(Comparator.comparingInt(User::getAge)
+                        .reversed()
+                        .thenComparing((User::getFirstName)))
+                .collect(Collectors.toList());
     }
 
     @Override
